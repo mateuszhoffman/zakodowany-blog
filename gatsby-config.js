a255b19b19f79397daf6f8b85744ba816ee0d2eb
Mateusz Hoffman <mateusz.hoffman@gmail.com>
2020-04-13 15:38:28 +0200

    Build debug turned on

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
         useACF: false,
         auth: {},
         // Set to true to debug endpoints on 'gatsby build'
-        verboseOutput: false,
+        verboseOutput: true,
       },
     },
     {
